da77115d1a63cd02f45642f7645b3d5ca86e0906
NosborRecaf <164422263+NosborRecaf@users.noreply.github.com>
2025-12-01 19:25:56 +1300

    Added jumping to 3D
    yeet

--- Paper Crown/Assets/TESTING/test scripts/Movement.cs
+++ Paper Crown/Assets/TESTING/test scripts/Movement.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
 
+// Added jumping and ground checking
+// RFA 1/12/2025
+
 public class Movement : MonoBehaviour
 {
     public float speed;
+    public float jumpHeight;
     public Rigidbody rb;
     public SpriteRenderer sr;
+    public bool grounded;
 
+    public CapsuleCollider groundCheck;
+    public LayerMask groundMask;
+    public Collider[] myList;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,11 +24,15 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        myList = Physics.OverlapSphere(groundCheck.transform.position, groundCheck.radius, groundMask);
+        // If the list has any ground layer items in it, the player is grounded
+        grounded = myList.Length > 0;
         float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-        Vector3 moveDirection = new Vector3(x, 0, y);
-        rb.linearVelocity = moveDirection * speed;
-
+        float z = Input.GetAxis("Vertical");
+        Vector3 moveDirection = new Vector3(x * speed, rb.linearVelocity.y, z * speed);
+        rb.linearVelocity = moveDirection;
+        
+        // Character flipping
         if (x < 0)
         {
             sr.flipX = true;
@@ -28,6 +40,12 @@ public class Movement : MonoBehaviour
         if (x > 0)
         {
             sr.flipX = false;
+        }
+        
+        // Jumping
+        if (Input.GetKey(KeyCode.Space) && grounded)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpHeight, rb.linearVelocity.z);
         }
     }
 }
