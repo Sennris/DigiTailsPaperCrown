950d103d919621047cec8743a609f1cc427fe982
NosborRecaf <164422263+NosborRecaf@users.noreply.github.com>
2025-12-02 23:46:58 +1300

    Worked a little more on sound manager update

--- Paper Crown/Assets/Scripts/Sound/SoundManager2.cs
+++ Paper Crown/Assets/Scripts/Sound/SoundManager2.cs
@@ -1,16 +1,24 @@
-using Unity.Collections;
-using UnityEngine;
-using System.Collections.Generic;
 using System;
+using System.Collections.Generic;
+using Unity.Collections;
 using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 public class SoundManager2 : MonoBehaviour
 {
+    public AudioSource audioSource;
+
     [SerializeField]
     SoundList2 soundList;
 
     [SerializeField]
     Dictionary<string, AudioClip> SoundList;
+
+    void Start()
+    {
+        
+    }
 }
 
 [Serializable]
@@ -23,8 +31,20 @@ public class SoundList2
 [Serializable]
 public class SoundListItem
 {
+    private void Awake()
+    {
+        var instance = this;
+    }
+
     [SerializeField]
     public string name;
     [SerializeField]
     public AudioClip[] audioClipList;
+
+    private AudioClip randomClip;
+
+    public static void PlaySound2(string category, float volume = 1)
+    {
+        //instance.audioClipList[0];
+    }
 }
